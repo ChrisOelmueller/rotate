6b04d1a15d8372d0db6e1b3e6e929212b0f898a2
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-02-28 12:47:16 +0100

    Three different routes for rotate, mirror, transpose
    Factors out most custom logic per operation. Also drops the separated
    result/output page and just displays it inline, preserving the input.
    Updates templates to read options and data based on current operation.

--- app.py
+++ app.py
@@ -7,37 +7,70 @@ from rotate import mirror, rotate, transpose
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+@app.route('/rotate', methods=['GET', 'POST'])
 def r():
     options = {
-        'operation': [(x, x.capitalize()) for x in ('rotate', 'mirror', 'transpose')],
         'rotation': [('positive', 'Counterclockwise'), ('negative', 'Clockwise')],
         'iterations': [(i, u'%s°' % (90*i)) for i in range(1, 4)],
+    }
+    text = output = ''
+    mode = 'rotate'
+
+    def parse_form(text):
+        iterations = int(request.form['iterations'])
+        clockwise = request.form['rotation'] == 'negative'
+        return do_rotate(text, iterations, clockwise=clockwise)
+
+    def do_rotate(mytext, iterations, clockwise=False):
+        if clockwise:
+            # Subtract from 360 degree rotation in other direction
+            iterations = 4 - iterations
+        for _ in range(iterations):
+            mytext = rotate(mytext)
+        return mytext
+
+    if request.method == 'POST':
+        text = request.form['text']
+        output = parse_form(text)
+
+    return render_template('submit.html', mode=mode, options=options, text=text, output=output)
+
+
+@app.route('/mirror', methods=['GET', 'POST'])
+def m():
+    options = {
         'axis': [('h', 'Horizontal'), ('v', 'Vertical')],
     }
-    if request.method == 'GET':
-        return render_template('submit.html', options=options)
-    elif request.method == 'POST':
-        text = request.form['text']
-        operation = request.form['operation']
-        if operation == 'rotate':
-             iterations = int(request.form['iterations'])
-             clockwise = request.form['clockwise'] == 'negative'
-             returned_text = do_rotate(text, iterations, clockwise=clockwise)
-        elif operation == 'mirror':
-             vertical_axis = request.form['axis'] == 'v'
-             returned_text = mirror(text, vertical=vertical_axis)
-        elif operation == 'transpose':
-             returned_text = transpose(text)
-        return render_template('output.html', text=returned_text, operation=operation)
+    text = output = ''
+    mode = 'mirror'
 
-def do_rotate(text, iterations, clockwise=False):
-    if clockwise:
-        # Subtract from 360 degree rotation in other direction
-        iterations = 4 - iterations
-    for _ in range(iterations):
-        text = rotate(text)
-    return text
+    def parse_form(text):
+        vertical_axis = request.form['axis'] == 'v'
+        return mirror(text, vertical=vertical_axis)
+
+    if request.method == 'POST':
+        text = request.form['text']
+        output = parse_form(text)
+
+    return render_template('submit.html', mode=mode, options=options, text=text, output=output)
+
+
+@app.route('/transpose', methods=['GET', 'POST'])
+def t():
+    options = {}
+    text = output = ''
+    mode = 'transpose'
+
+    def parse_form(text):
+        return transpose(text)
+
+    if request.method == 'POST':
+        text = request.form['text']
+        output = parse_form(text)
+
+    return render_template('submit.html', mode=mode, options=options, text=text, output=output)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
